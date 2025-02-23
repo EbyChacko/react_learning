f052f60b7e3453748ab9e20abd2c39d790ad4b52
Eby <cybe1990@gmail.com>
2025-02-23 08:09:06 +0000

    add css for the heading

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>30 Days React Learning Challenge</h1>
+        <h1 className='heading'>30 Days React Learning Challenge</h1>
         <Day1 name="Eby Chacko" role="Full-Stack Developer" />
       </header>
     </div>
