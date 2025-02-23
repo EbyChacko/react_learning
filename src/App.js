2fc976c08469adbe79f18afa26ec52a2c309f700
Eby <cybe1990@gmail.com>
2025-02-23 07:49:00 +0000

    create a profile for the day1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Day1 } from './components/Day1.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>30 Days React Learning Challenge</h1>
+        <Day1 name="Eby Chacko" role="Full-Stack Developer" />
       </header>
     </div>
   );
---
+++ src/components/Day1.js
@@ -0,0 +1,10 @@
+export function Day1({ name, role }) {
+    return (
+      <div className="wrapper">
+        <h4>Day 1</h4>
+        <h2>{name}</h2>
+        <p>{role}</p>
+      </div>
+    );
+  }
+  
