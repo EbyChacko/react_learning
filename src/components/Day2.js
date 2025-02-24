daf34f5a1ad0f550b2a4b5f3649d1a5977d9affb
Eby <cybe1990@gmail.com>
2025-02-24 20:14:03 +0000

    add dynamically changing name input

--- src/components/Day2.js
+++ src/components/Day2.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 export default function Day2() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
 
     return (
         <div className="wrapper">
@@ -10,7 +11,9 @@ export default function Day2() {
             <p>{count}</p>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
-            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={() => setCount(0)}>Reset</button><br></br>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <p className='day2Name'>{name}</p>
         </div>  
     );
 }
