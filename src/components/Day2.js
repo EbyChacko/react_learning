a7340c47e401ba265776ae5eaef20c9e5717fbd3
Eby <cybe1990@gmail.com>
2025-02-24 20:18:15 +0000

    add label for name

--- src/components/Day2.js
+++ src/components/Day2.js
@@ -8,12 +8,13 @@ export default function Day2() {
         <div className="wrapper">
             <h4 className="days">Day 2</h4>
             <h2>Counter</h2>
-            <p>{count}</p>
+            <p>Count : {count}</p>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button><br></br>
+            <label>Enter your name</label>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            <p className='day2Name'>{name}</p>
+            <p className='day2Name'>Your Name : {name}</p>
         </div>  
     );
 }
